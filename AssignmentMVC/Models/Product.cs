e603ce02d10815d1a9b69b38768e51b80068c12f
Abhijitakhande <abhijitakhande873@gmail.com>
2026-02-25 12:28:56 +0530

    Added server-side validation for product name length

--- AssignmentMVC/Models/Product.cs
+++ AssignmentMVC/Models/Product.cs
@@ -1,8 +1,12 @@
-﻿namespace AssignmentMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentMVC.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [StringLength(50)]
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
